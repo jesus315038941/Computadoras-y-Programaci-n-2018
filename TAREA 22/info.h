08519d775d34190ddeae9f504a124eee69139f74
José de Jesús Mendoza Valdivia <jesus.h.2521@gmail.com>
2017-11-16 09:54:46 -0600

    Programas en C "punteros"

---
+++ TAREA 22/info.h
@@ -0,0 +1,10 @@
+void appInfoData(char *titulo,char *fecha)
+{
+  printf("----------------------------------------------------------------\n");
+  printf("_\n");
+  printf("_       %s\n", titulo);
+  printf("_       Jose de Jesus Mendoza Valdivia ®\n");
+  printf("_       %s\n", fecha);
+  printf("_\n");
+  printf("----------------------------------------------------------------\n");
+}
